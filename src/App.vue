e44ca339819ef96cf8dc9929444dd013779bd5de
Martin Stefek <42932365+martinstefek@users.noreply.github.com>
2024-04-19 17:27:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>My Page</div>
+</template>
+
+<script setup lang="ts">
+import { useAuthorization } from '@testlib/my-library/common';
+
+const {} = useAuthorization();
+</script>
+
+<style lang="scss"></style>
